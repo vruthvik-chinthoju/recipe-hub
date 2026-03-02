80811f82597b56677ed5f087670b2da43d72d4da
ennter usernane <vruthvik.chinthoju@gmail.com>
2026-03-02 19:50:25 +0530

    Updated production settings

--- cookrecipe/settings.py
+++ cookrecipe/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = os.getenv("SECRET_KEY")
 import os
 
-SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-local-key")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
@@ -133,20 +133,15 @@ LOGOUT_REDIRECT_URL = "signin"
 LOGIN_URL = "signin"
 
 
+SECRET_KEY = os.environ["SECRET_KEY"]
 
-# DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-DEBUG = True
-
-# ALLOWED_HOSTS = [
-#     "localhost",
-#     "127.0.0.1",
-#     ".onrender.com",
-# ]
-
-ALLOWED_HOSTS = []
-
-
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    ".onrender.com",
+]
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
